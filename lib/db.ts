73bb6ee134fdcb447f3a78e8c166977643bd4ead
JoseAl05 <jparancibialinker@gmail.com>
2023-10-27 05:40:45 -0300

    changed db url

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,20 @@
 import { PrismaClient } from '@prisma/client';
 
 declare global {
-    var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db =
+  globalThis.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url:
+          process.env.NODE_ENV === 'development'
+            ? process.env.DATABASE_URL_DEV
+            : process.env.DATABASE_URL_PROD,
+      },
+    },
+  });
 
-if(process.env.NODE_ENV !== 'production') globalThis.prisma = db
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
