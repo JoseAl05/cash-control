45166006fff3692a45a4f9d34fade71325516c13
JoseAl05 <jparancibialinker@gmail.com>
2023-11-22 02:43:49 -0300

    Add a chart of expenses (BETA)

--- app/(routes)/gastos/page.tsx
+++ app/(routes)/gastos/page.tsx
@@ -1,17 +1,22 @@
+import ExpenseChart from '@/components/charts/ExpenseChart';
 import { Button } from '@/components/ui/button';
+import { db } from '@/lib/db';
 import Link from 'next/link';
 
-const ExpensesPage = () => {
+const ExpensesPage = async () => {
+
+    const expenses = await db.expense.findMany();
+    const incomes = await db.income.findMany();
+
     return (
         <div className='flex flex-col items-center justify-center gap-y-5 h-screen'>
             <h1 className='text-5xl'>
                 Gastos
             </h1>
-            <Link href='/'>
-                <Button variant='primary' size='lg'>
-                    Ir al Home.
-                </Button>
-            </Link>
+            <ExpenseChart
+                expenses={expenses}
+                incomes={incomes}
+            />
         </div>
     );
 }
